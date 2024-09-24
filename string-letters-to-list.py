678fe00c2d52d9238197376d43f139c41e036213
kkpncc <kkpncc01@gmail.com>
2024-09-24 22:10:55 +0530

    Create string-letters-to-list.py

---
+++ string-letters-to-list.py
@@ -0,0 +1,6 @@
+s = input("enter the string")   # s is the name of string
+lst =[]                         # lst name of lst
+for a in s:                     # a is iteration variable
+    lst.append(a)
+    #print(lst)                 # use it see progress a lst gets updated
+print(lst)
